660aa325fe321f14ad7212b50a25458305cfc53d
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-05 16:02:11 +0200

    update variables chapt 2 ex 2

--- js-complete-course/02-colors/03-change-bcg-three/script.js
+++ js-complete-course/02-colors/03-change-bcg-three/script.js
@@ -15,7 +15,7 @@
 
     generate.addEventListener('click',  function onClick() {
 
-        let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
         document.body.style.backgroundColor = randomColor;
 
         console.log(randomColor);
