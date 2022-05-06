d547cfdb9e229ddecd1aeb4b46449623b96bb73b
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-06 17:10:42 +0200

    Finished the first exercise of chapter 4

--- js-complete-course/04-maths/01-calculator-one/script.js
+++ js-complete-course/04-maths/01-calculator-one/script.js
@@ -12,19 +12,39 @@
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
+    // Unary plus (+) precedes its operand and evaluates to its operand but attempts to convert it into a number, if it isn't already.
+
     document.getElementById("addition").addEventListener("click", function() {
         // perform an addition
+        let one = +document.getElementById("op-one").value;
+        let two = +document.getElementById("op-two").value;
+        let addition = one + two;
+        console.log(addition);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let one = +document.getElementById("op-one").value;
+        let two = +document.getElementById("op-two").value;
+        let subtraction = one - two;
+        console.log(subtraction);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let one = +document.getElementById("op-one").value;
+        let two = +document.getElementById("op-two").value;
+        let multiplication = one * two;
+        console.log(multiplication);
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        // let one = +document.getElementById("op-one").value;
+        // let two = +document.getElementById("op-two").value;
+        let one = parseInt(document.getElementById("op-one").value);
+        let two = parseInt(document.getElementById("op-two").value);
+        let division = one / two;
+        console.log(division);
     });
 })();
