8b7586369a52c04209b679a62da16602e68b4d3d
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-06 14:10:08 +0200

    finished exerc 3 of chapt 3 had some help of Lucas

--- js-complete-course/03-dates/03-age-by-select/script.js
+++ js-complete-course/03-dates/03-age-by-select/script.js
@@ -11,6 +11,30 @@
 
 (function() {
 
-    // your code here
+    let btn = document.getElementById('run');
+
+    btn.addEventListener('click', function onClick() {
+        let inputDay = document.getElementById('dob-day').value;
+        let inputMonth = document.getElementById('dob-month').value;
+        let inputYear = document.getElementById('dob-year').value;
+
+        let inputDate = `${inputDay}-${inputMonth}-${inputYear}`;
+        let age = ageCalc(inputDate);
+
+        console.log(age);
+
+        function ageCalc(birthday) {
+
+            let date = new Date();
+            let dateOfBirth = new Date(birthday);
+            let age = date.getFullYear() - dateOfBirth.getFullYear();
+            let month = date.getMonth() - dateOfBirth.getMonth();
+            if (month < 0 || (month === 0 && date.getDate() < dateOfBirth.getDate())) {
+                age--;
+            }
+            return age;
+        }
+
+    });
 
 })();
