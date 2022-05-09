a7afb6f4ef8db6b04d546eb91cd47db5172fd12c
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-09 16:00:42 +0200

    finished exerc 2 of chapter 5, adding and deleting elements in an array

--- js-complete-course/05-arrays/02-manipulate-array/script.js
+++ js-complete-course/05-arrays/02-manipulate-array/script.js
@@ -9,9 +9,21 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE: When you click the button, do the following transformations to the array fruits:
+    remove the first and the last element, add an element "banana" at the start of the array,
+    and add an element "kiwi" at the end.*/
+
+/*
+    INFORMATION about the used methods can be found at:
+    developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift    -   removing the first element of an array
+    developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop      -   removing the last element of an array
+    developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift  -   adds an element to the beginning of an array
+    developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push     -   adds an element to the end of an array
+ */
+
 (function() {
 
-    var fruits = [
+    let fruits = [
         "pomme",
         "poire",
         "fraise",
@@ -25,5 +37,17 @@
     ];
 
     // your code here
+    document.getElementById('run').addEventListener("click", function (){
+        console.log('fruits before:', JSON.stringify(fruits));
+        //array.shift removes the first element from an array
+        fruits.shift();
+        //array.unshift adds one or more elements to the beginning of an array
+        fruits.unshift('banana');
+        //array.pop removes the last element from an array
+        fruits.pop();
+        //array.push adds one or more elements to the end of an array
+        fruits.push('kiwi');
+        console.log('fruits after:', fruits);
+    })
 
 })();
