853603958c2e612ed76f7be1d1247abe7992a166
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-10 18:22:30 +0200

    Finished exercise 4, chapter 6, be sure to re-read the information about this one

--- js-complete-course/06-dom/04-attr-create/script.js
+++ js-complete-course/06-dom/04-attr-create/script.js
@@ -9,8 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE:
+ *      Get the value of the "data-image" attribute from the element with id "source".
+ *      Create a new image element inside the target element that displays the image referred to by the attribute value.
+ *      Then remove the original element.
+ *
+ *  INFORMATION:
+ *      developer.mozilla.org/en-US/docs/Web/API/Element/append
+ *      developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute
+ *      developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+ *      developer.mozilla.org/en-US/docs/Web/API/Element/remove
+ *
+ *  REMINDER:
+ *      Double check the information, this exercise was a tough one to crack.
+ */
+
 (function() {
 
     // your code here
+
+    // a variable source that gets the value of the data-image attribute from id source
+    let source = document.getElementById('source').getAttribute('data-image');
+    // creating a new img element
+    let image = document.createElement('img');
+    // add the source for the image
+    image.src = source;
+    // adding the image to our target id
+    document.getElementById('target').append(image);
+    // removing the original element
+    document.getElementById('source').remove();
 
 })();
