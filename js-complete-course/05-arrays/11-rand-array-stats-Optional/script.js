5d27eb117ff9bf715b9dee0db0ce34533be40146
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-10 12:34:43 +0200

    Just started excersise 11 of chapter 5, made an array with 10 random numbers

--- js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
+++ js-complete-course/05-arrays/11-rand-array-stats-Optional/script.js
@@ -9,8 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE:
+        When you click the button, display the ten elements of an array of 10 numbers (between 1 and 100) in the table.
+        Then display in the definition list: the smallest number, the biggest number, the sum of all numbers, and their average.
+ */
+
+/*  INFORMATION:
+        developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+ */
+
 (function() {
 
     // your code here
+    const randomNumbers = [];
+
+    for (i=0; i<10; i++){
+        randomNumbers.push(Math.floor(Math.random()*100));
+    }
+
+    document.getElementById('run').addEventListener("click", function (){
+        console.log(randomNumbers);
+
+    });
 
 })();
