4e8c1072909f865e0a50ca87b7bd0b98e784b9b2
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-10 16:33:54 +0200

    Finished exercise 3 of chapter 6, finally owned it mmmm love the feeling of a good learning headache

--- js-complete-course/06-dom/03-select-three/script.js
+++ js-complete-course/06-dom/03-select-three/script.js
@@ -9,8 +9,22 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE:
+ *      Select the elements with the class "target" and replace their content with "owned"
+ *
+ *  INFORMATION:
+ *      developer.mozilla.org/en-US/docs/Web/API/Document/getElementsByClassName
+ *      developer.mozilla.org/en-US/docs/Web/API/HTMLElement/innerText
+ *      developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration#for_statement
+ */
+
 (function() {
 
     // your code here
+
+    const getClass = document.getElementsByClassName('target');
+    for (let i = 0; i < getClass.length; i++){
+        getClass[i].innerText = "Owned";
+    }
 
 })();
