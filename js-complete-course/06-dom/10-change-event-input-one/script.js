9f4268d89bb32fe771dd49465b1d4dc8790ae437
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-11 16:48:55 +0200

    Started with exercise 10, chapter 6, only added a let, will work on the rest later/tomorrow

--- js-complete-course/06-dom/10-change-event-input-one/script.js
+++ js-complete-course/06-dom/10-change-event-input-one/script.js
@@ -9,8 +9,19 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE:
+ *      Listen for changes in the input field and display the number of characters entered in the counter span.
+ *      As soon as 10 characters have been entered, don't allow any more characters to be typed.
+ *
+ *  INFORMATION:
+ *
+ */
+
 (function() {
 
     // your code here
+    let input = document.getElementById('pass-one');
+
+
 
 })();
