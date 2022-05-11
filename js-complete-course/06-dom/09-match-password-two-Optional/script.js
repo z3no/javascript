31de0cf6bd29d78e6e5bf9ed875b40c23764d886
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-11 15:11:34 +0200

    Finished exercise 9, chapter 6, className

--- js-complete-course/06-dom/09-match-password-two-Optional/script.js
+++ js-complete-course/06-dom/09-match-password-two-Optional/script.js
@@ -9,8 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE:
+ *      When you click the button, check whether the two passwords entered in the input fields are exactly the same.
+ *      If they are not, add the class "error" to the input boxes.
+ *
+ *  INFORMATION:
+ *      https://developer.mozilla.org/en-US/docs/Web/API/Element/className
+ */
+
 (function() {
 
     // your code here
+    document.getElementById('run').addEventListener("click", function (){
+        const password1 = document.getElementById('pass-one');
+        const password2 = document.getElementById('pass-two');
+
+        if (password1.value === password2.value) {
+            console.log('true');
+        } else {
+            password1.className = "error";
+            password2.className = "error";
+        }
+
+    })
 
 })();
