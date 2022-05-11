a2763f5db4b5d5fa4b70b6f1196d59b09013638f
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-11 14:03:56 +0200

    Finished exercise 7 (optional) of chapter 6

--- js-complete-course/06-dom/07-generate-table-two-Optional/script.js
+++ js-complete-course/06-dom/07-generate-table-two-Optional/script.js
@@ -9,8 +9,33 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*  CHALLENGE:
+ *      Inside the element with id "target", create an HTML table with 10 columns and 10 rows, containing the tables of multiplication from 1 to 10.
+ *
+ *  INFORMATION:
+ *      See exercise 06 of chapter 06 Dom, think logical about what kind of calculation u have to make for the tables of multiplication.
+ */
+
 (function() {
 
     // your code here
+    const target = document.getElementById('target');
+
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+
+    for (let i = 1; i <= 10; i++) {
+        const row = document.createElement('tr');
+
+        for (let j = 1; j <= 10; j++) {
+            const column = document.createElement('td');
+            const content = document.createTextNode(`${i*j}`);
+            column.appendChild(content);
+            row.appendChild(column);
+        }
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+    target.appendChild(table);
 
 })();
