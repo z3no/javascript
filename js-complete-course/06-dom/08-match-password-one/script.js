4cc5daaddd88737d98e9cc1c905ec78c72a56497
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-11 14:47:54 +0200

    Finished exercise 8, chapter 6, added a target id in the index.html to show a message when the passwords are identical, input boxes border turn red when they are not identical

--- js-complete-course/06-dom/08-match-password-one/script.js
+++ js-complete-course/06-dom/08-match-password-one/script.js
@@ -9,8 +9,28 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+/*  CHALLENGE:
+ *      When you click the button, check whether the two passwords entered in the input fields are exactly the same.
+ *      If they are not, change the border color of the input boxes to be red.
+ *
+ *  INFORMATION:
+ *
+ */
+
+(function password() {
 
     // your code here
+    document.getElementById('run').addEventListener("click", function (){
 
+        const password1 = document.getElementById('pass-one');
+        const password2 = document.getElementById('pass-two');
+
+        if (password1.value === password2.value) {
+            document.getElementById('target').innerHTML = "Your passwords match!";
+        } else {
+            password1.style.border = '1px solid red';
+            password2.style.border = '1px solid red';
+            document.getElementById('target').innerHTML = "Your passwords don't match!"
+        }
+    })
 })();
