d478e64c5ade65e85123a02b9e8e4ea780f5c579
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-12 15:50:59 +0200

    fetching the heroes by their id input and displaying the name in the tag with id target

--- js-complete-course/07-fetch/02-details/script.js
+++ js-complete-course/07-fetch/02-details/script.js
@@ -9,6 +9,37 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*
+ *  CHALLENGE:
+ *      When you click on the button, get the id from the form, then get the corresponding X-Men from the API and display it in the tag whose id is "target".
+ *      Use the tag template to reproduce a suitable html structure.
+ */
+
 (() => {
     // your code here
+    const button = document.getElementById('run');
+    const target = document.getElementById('target');
+    const template = document.querySelector('#tpl-hero');
+
+    button.addEventListener("click", function (){
+        getXMenById();
+    });
+    // Defining async function
+
+    async function getXMenById () {
+        const heroId = document.getElementById('hero-id').value;
+        // Storing response
+        const response = await fetch (`http://localhost:3000/heroes/${heroId}`);
+        // If the call failed, give an error
+        if (!response.ok) {
+            throw 'Something went wrong!';
+        }
+        // Storing data in form of JSON
+        const data = await response.json();
+
+        // Showing the name of the hero
+        target.innerText = data.name;
+    }
+
+
 })();
