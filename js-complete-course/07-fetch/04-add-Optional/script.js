dd8157578230bec9290103276bb0088965b826ca
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-13 17:18:18 +0200

    Trying to create an array, that gets the values from the input, and then push it to the api, baby steps

--- js-complete-course/07-fetch/04-add-Optional/script.js
+++ js-complete-course/07-fetch/04-add-Optional/script.js
@@ -9,6 +9,38 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*
+ *  CHALLENGE:
+ *      When you click on the button, retrieve the values of the form and create the new hero in the API.
+ *      Each field must be filled in (it is up to you to manage).
+ *      Display the result of all your heroes in the console.
+ *
+ *      Caution: powers must be stored in the same format as the previous heroes!
+ *      You do not have to update the JSON file, it is oké that when you refresh the page you lose the newly added heroes.
+ *
+ *  INFORMATION:
+ *
+ */
+
 (() => {
     // your code here
+    const button = document.getElementById('run');
+    const API_url = ('http://localhost:3000/heroes');
+
+    async function getApi() {
+        const response = await fetch(API_url);
+        if (!response.ok) {
+            throw 'Something went wrong, friend'
+        }
+        const data = await response.json();
+    }
+
+    let newHeroArray = [];
+
+    const nameInput = document.getElementById('hero-name').value;
+    const alterEgoInput = document.getElementById('hero-alter-ego').value;
+    const powersInput = document.getElementById('hero-powers').value;
+
+    button.addEventListener("click", getApi);
+
 })();
