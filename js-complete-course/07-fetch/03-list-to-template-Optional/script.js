46e4f685783bbf511beeb6379b6635b9cf678bcf
Zeno Driesen <zeno.driesen@gmail.com>
2022-05-13 11:54:35 +0200

    Finished exercise 3, chapter 7, get xmen from api, cloned template to target and displayed the xmen on click

--- js-complete-course/07-fetch/03-list-to-template-Optional/script.js
+++ js-complete-course/07-fetch/03-list-to-template-Optional/script.js
@@ -9,6 +9,52 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+/*
+ *  CHALLENGE:
+ *      When you click on the button, use the fetch function, retrieve the list of X-Men and display it in the tag whose id is "target".
+ *      Use the template tag to reproduce an html structure for each hero.
+ *      The template tag already exists in the HTML structure, be sure to check the HTML code!
+ *
+ *  INFORMATION:
+ *      https://developer.mozilla.org/en-US/docs/Web/API/Response/ok
+ *      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/throw
+ *      https://www.w3schools.com/jsref/met_node_clonenode.asp
+ *      https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
+ *      https://developer.mozilla.org/en-US/docs/Web/API/Node/appendChild
+ */
+
 (() => {
     // your code here
+    const target = document.getElementById('target');
+    const template = document.getElementById('tpl-hero').content;
+
+    async function getXMenAPI() {
+        const response = await fetch ('http://localhost:3000/heroes')
+        if (!response.ok) {
+            throw 'Something went wrong!'
+        }
+        const data = await response.json();
+        return data;
+    }
+
+    async function displayXMen() {
+        const heroes = await getXMenAPI()
+        heroes.forEach(xmen => {
+            const clone = template.cloneNode(true);
+
+            const name = clone.querySelector('.name');
+            name.textContent = xmen.name;
+
+            const alterEgo = clone.querySelector('.alter-ego');
+            alterEgo.textContent = ` aka ${xmen.alterEgo}`;
+
+            const powers = clone.querySelector('.powers');
+            powers.textContent = xmen.abilities;
+
+            target.appendChild(clone);
+        })
+    }
+
+    document.getElementById('run').addEventListener("click", displayXMen);
+
 })();
